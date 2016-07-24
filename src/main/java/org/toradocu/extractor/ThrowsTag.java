6951bc22a599ee5e33b2545fb3504786d7b2af2b
Anmol Jammu <jammua@cs.washington.edu>
2016-07-24 11:35:42 -0700

    Refactor and add Javadoc to util, extractor packages; parse @param annotations

--- src/main/java/org/toradocu/extractor/ThrowsTag.java
+++ src/main/java/org/toradocu/extractor/ThrowsTag.java
@@ -2,10 +2,22 @@ package org.toradocu.extractor;
 
 import java.util.Objects;
 
+/**
+ * This class represents a throws tag in a method.
+ */
 public final class ThrowsTag {
-	private final String comment;
-	private final String exception;
 	
+	/** The fully qualified name of the exception. */
+	private final String exception;
+	/** The comment associated with the exception. */
+	private final String comment;
+	
+	/**
+	 * Constructs a {@code ThrowsTag} with the given exception and comment.
+	 * 
+	 * @param exception the fully qualified name of the exception
+	 * @param comment the comment associated with the exception
+	 */
 	public ThrowsTag(String exception, String comment) {
 		Objects.requireNonNull(exception);
 		Objects.requireNonNull(comment);
@@ -13,14 +25,30 @@ public final class ThrowsTag {
 		this.exception = exception;
 	}
 	
+	/**
+	 * Returns the comment associated with the exception in this throws tag.
+	 * 
+	 * @return the comment associated with the exception in this throws tag
+	 */
 	public String getComment() {
 		return comment;
 	}
 	
+	/**
+	 * Returns the fully qualified name of the exception in this throws tag.
+	 * 
+	 * @return the fully qualified name of the exception in this throws tag
+	 */
 	public String getException() {
 		return exception;
 	}
 	
+	/**
+	 * Returns true if this {@code ThrowsTag} and the specified object are equal.
+	 * 
+	 * @param obj the object to test for equality
+	 * @return true if this object and {@code obj} are equal
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof ThrowsTag)) return false;
@@ -29,11 +57,23 @@ public final class ThrowsTag {
 		return this.comment.equals(that.comment) && this.exception.equals(that.exception);
 	}
 	
+	/**
+	 * Returns the hash code of this object.
+	 * 
+	 * @return the hash code of this object
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(comment, exception);
 	}
 	
+	/**
+	 * Returns a string representation of this throws tag. The returned string is in the
+	 * format "@throws EXCEPTION COMMENT" where EXCEPTION is the fully qualified name of
+	 * the exception in this throws tag and COMMENT is the text of the comment in the throws tag.
+	 * 
+	 * @return a string representation of this throws tag
+	 */
 	@Override
 	public String toString() {
 		return "@throws " + exception + " " + comment;
--- src/main/java/org/toradocu/util/Builder.java
+++
@@ -1,5 +0,0 @@
-package org.toradocu.util;
-
-public interface Builder<T> {
-	public T build();
-}
