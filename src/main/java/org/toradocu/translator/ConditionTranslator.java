65414fbba36d06142ffc657d0ed8999cfd5483bc
Anmol Jammu <jammua@cs.washington.edu>
2016-08-09 22:54:20 -0700

    Tweak build configuration and remove unused imports

--- src/main/java/org/toradocu/translator/ConditionTranslator.java
+++ src/main/java/org/toradocu/translator/ConditionTranslator.java
@@ -32,18 +32,20 @@ public class ConditionTranslator {
 				StringBuilder logMessage = new StringBuilder("===" + method.getSignature() + "===");
 				logMessage.append("\n").append("Identifying propositions from: \"" + tag.getComment() + "\"");
 				LOG.info(logMessage.toString());
-				
+
 				Set<String> conditions = new HashSet<>();
 				// Identify propositions in the comment. Each sentence in the comment is parsed into
 				// a PropositionSeries.
 				List<PropositionSeries> extractedPropositions
-						= PropositionExtractor.getPropositionSeries(tag.getComment());		
+						= PropositionExtractor.getPropositionSeries(tag.getComment());
+				LOG.trace(extractedPropositions.toString());
 
 				// Identify Java code elements in propositions.
 				for (PropositionSeries propositions : extractedPropositions) {
-					translatePropositions(propositions, method);
+					translatePropositions(propositions, method, tag);
 				}
-				
+
+				/*
 				// We remove from the proposition graph all the proposition for which the translation has failed
 //				pruneUntranslatedPropositions(propositionGraph);
 				
@@ -62,6 +64,7 @@ public class ConditionTranslator {
 //						conditions.add(p.getTranslation().get());
 //					}
 //				}
+				*/
 				tag.setConditions(conditions);
 			}
 		}
@@ -92,7 +95,7 @@ public class ConditionTranslator {
 //		}
 //	}
 
-	private static void translatePropositions(PropositionSeries propositionSeries, DocumentedMethod method) {
+	private static void translatePropositions(PropositionSeries propositionSeries, DocumentedMethod method, ThrowsTag tag) {
 	
 		for (Proposition p : propositionSeries.getPropositions()) {
 			String translation = "";
@@ -147,5 +150,5 @@ public class ConditionTranslator {
 			return "";
 		}
 	}
-
+*/
 }
