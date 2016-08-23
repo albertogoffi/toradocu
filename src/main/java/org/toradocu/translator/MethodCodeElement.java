4a42955cb621faf56f302bac4efe9bff53448830
Anmol Jammu <jammua@cs.washington.edu>
2016-08-22 17:15:38 -0700

    Refactor MethodCodeElement

--- src/main/java/org/toradocu/translator/MethodCodeElement.java
+++ src/main/java/org/toradocu/translator/MethodCodeElement.java
@@ -1,21 +1,51 @@
 package org.toradocu.translator;
 
-public class MethodCodeElement extends CodeElement {
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
-	private final String receiver, signature;
+/**
+ * This class represents a method code element for use in translation. It holds String identifiers for the
+ * method and a Java expression representation of the class to build Java conditions.
+ */
+public class MethodCodeElement extends CodeElement<Method> {
+
+	/** The class/object in which this method is called. */
+	private final String receiver;
 	
-	public MethodCodeElement(String signature, String identifier) {
-		this(signature, "", identifier);
-	}
-
-	public MethodCodeElement(String signature, String receiver, String identifier) {
-		super(identifier);
-		this.signature = signature;
+	/**
+	 * Constructs and initializes a {@code MethodCodeElement} that identifies the given method. The given method
+	 * must take no arguments.
+	 * 
+	 * @param receiver the class/object in which the method is called
+	 * @param method the no-argument method that this code element identifies
+	 * @throws IllegalArgumentException if method takes arguments
+	 */
+	public MethodCodeElement(String receiver, Method method) {
+		super(method);
+		if (method.getParameterCount() > 0) {
+			throw new IllegalArgumentException("Method '" + method.getName() + "' has " + method.getParameterCount() +
+											   " parameters. Expected 0.");
+		}
 		this.receiver = receiver;
+		
+		// Add name identifier.
+		String methodName = method.getName();
+		if (methodName.startsWith("get")) {
+			methodName = methodName.replaceFirst("get", "");
+		}
+		addIdentifier(methodName);
 	}
 
+	/**
+	 * Builds and returns the Java expression representation of this method code element. The returned
+	 * string is formatted as "RECEIVER.SIGNATURE" where RECEIVER is the name of the class in which this
+	 * method is called and SIGNATURE is the signature of this method.
+	 * 
+	 * @return the Java expression representation of this method code element after building it
+	 */
 	@Override
 	protected String buildJavaExpression() {
-		return receiver + "." + signature;
+		return receiver + "." + getJavaCodeElement().getName() + "()";
 	}
+
 }
