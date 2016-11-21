9e7606d8398827d1d684a722572dc2f0f5cb47d0
Alberto Goffi <alberto.goffi@usi.ch>
2016-11-21 21:19:02 +0100

    Add FreeCol to precision/recall test suite

--- src/main/java/org/toradocu/translator/Matcher.java
+++ src/main/java/org/toradocu/translator/Matcher.java
@@ -246,8 +246,9 @@ public class Matcher {
    */
   protected static String simpleMatch(String predicate) {
     String verbs = "(is|are|is equal to|are equal to|equals to) ?";
+
     String predicates =
-        "(true|false|null|this|zero|positive|strictly positive|negative|strictly " + "negative)";
+        "(true|false|null|this|zero|positive|strictly positive|negative|strictly negative)";
 
     java.util.regex.Matcher isPattern =
         Pattern.compile(verbs + "(==|=)? ?" + predicates).matcher(predicate);
@@ -266,8 +267,8 @@ public class Matcher {
       if (lastWord.equals("true") || lastWord.equals("false") || lastWord.equals("null")) {
         return "==" + lastWord;
       } else if (lastWord.equals("this")) {
-        //the target object in the generated Aspect class
-        return "==" + "target";
+        // The receiver object in the generated aspects.
+        return "==target";
       } else if (lastWord.equals("zero")) {
         return "==0";
       } else if (lastWord.equals("positive") || lastWord.equals("strictly positive")) {
