a9f1123d73c9229a1d42e18cf7d3541bcf0f9c16
Alberto Goffi <alberto.goffi@usi.ch>
2016-11-25 11:05:46 +0100

    Improve precision/recall summary

--- stats/precision_recall_summary.sh
+++ stats/precision_recall_summary.sh
@@ -19,7 +19,9 @@ fi
 # Run Toradocu and collect statistics
 ./gradlew test --tests "org.toradocu.PrecisionRecall*"
 echo "TOTAL,,,=SUM(D1:INDIRECT(\"D\" & ROW()-1)),=SUM(E1:INDIRECT(\"E\" & ROW()-1)),=SUM(D1:INDIRECT(\"F\" & ROW()-1)),," >> $STATS_FILE
-echo "AVERAGE PRECISION,=INDIRECT(\"D\" & ROW()-1)/(INDIRECT(\"D\" & ROW()-1) + INDIRECT(\"E\" & ROW()-1))" >> $STATS_FILE
-echo "AVERAGE RECALL,=INDIRECT(\"D\" & ROW()-2)/(INDIRECT(\"D\" & ROW()-2) + INDIRECT(\"E\" & ROW()-2) + INDIRECT(\"F\" & ROW()-2))" >> $STATS_FILE
+echo "NUMBER OF METHODS,=ROW()-2" >> $STATS_FILE
+echo "NUMBER OF CONDITIONS,=INDIRECT(\"D\" & ROW()-2)+INDIRECT(\"E\" & ROW()-2)+INDIRECT(\"F\" & ROW()-2)" >> $STATS_FILE
+echo "AVERAGE PRECISION,=INDIRECT(\"D\" & ROW()-3)/(INDIRECT(\"D\" & ROW()-3) + INDIRECT(\"E\" & ROW()-3))" >> $STATS_FILE
+echo "AVERAGE RECALL,=INDIRECT(\"D\" & ROW()-4)/INDIRECT(\"B\" & ROW()-2)" >> $STATS_FILE
 
 echo "Open the result file: $STATS_FILE"
