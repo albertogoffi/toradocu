0aa6b80d98a36e4a9f73943acd484cba3076fbf2
khaeghar <sergiodelgadocastellanos@gmail.com>
2016-12-05 11:17:18 +0100

    Formatted

--- src/main/java/org/toradocu/extractor/InterfaceTag.java
+++ src/main/java/org/toradocu/extractor/InterfaceTag.java
@@ -4,13 +4,15 @@ import java.util.Optional;
 
 public interface InterfaceTag {
 
-	public String toString();
-	public int hashCode();
-	public boolean equals(Object obj);
-	public Optional<String> getCondition();
-	public void setCondition(String s);
-	public String getComment();
-	
-	
-	
+  public String toString();
+
+  public int hashCode();
+
+  public boolean equals(Object obj);
+
+  public Optional<String> getCondition();
+
+  public void setCondition(String s);
+
+  public String getComment();
 }
