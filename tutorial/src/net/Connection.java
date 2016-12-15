e843c3fc4fc2f57e97687d7ed986101825939326
Michael Ernst <mernst@cs.washington.edu>
2016-12-15 09:14:19 -0800

    Make code look a bit more realistic

--- tutorial/src/net/Connection.java
+++ tutorial/src/net/Connection.java
@@ -1,8 +1,14 @@
 package net;
 
+/** A network connection. */
 public class Connection {
 
   private boolean open = false;
+
+  /** @returns true if the connection is open, false otherwise */
+  public boolean isOpen() {
+    return open;
+  }
 
   /**
    * Establishes a new connection.
@@ -11,12 +17,10 @@ public class Connection {
    */
   public void open() {
     // Establish a new connection.
-    open = true;
-  }
 
-  /** @returns true if the connection is open, false otherwise */
-  public boolean isOpen() {
-    return open;
+    // ... code elided ...
+
+    open = true;
   }
 
   /**
@@ -31,5 +35,7 @@ public class Connection {
     }
 
     // Send the message.
+
+    // ... code elided ...
   }
 }
