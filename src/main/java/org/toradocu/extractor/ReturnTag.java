aeeae9e4134395c81544946f53727519612dbc1e
khaeghar <sergiodelgadocastellanos@gmail.com>
2017-02-07 11:03:58 +0100

    Erased useless methods

--- src/main/java/org/toradocu/extractor/ReturnTag.java
+++ src/main/java/org/toradocu/extractor/ReturnTag.java
@@ -1,6 +1,5 @@
 package org.toradocu.extractor;
 
-import java.util.Objects;
 
 /**
  * This class represents a throws tag in a method. Each @throws tag consists of an exception, a
@@ -28,32 +27,6 @@ public class ReturnTag extends AbstractTag {
   public boolean equals(Object obj) {
     if (!(obj instanceof ReturnTag)) return false;
 
-    ReturnTag that = (ReturnTag) obj;
-    return this.getComment().equals(that.getComment()) && super.equals(that);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode());
-  }
-
-  /**
-   * Returns a string representation of this return tag. The returned string is in the format
-   * "@return COMMENT" where COMMENT is the text of the comment in the return tag. If translation
-   * has been attempted on this tag, then the returned string is also appended with " ==&gt;
-   * CONDITION" where CONDITION is the translated condition for the exception as a Java expression
-   * or the empty string if translation failed.
-   *
-   * @return a string representation of this throws tag
-   */
-  @Override
-  public String toString() {
-    String result = super.getKind() + " " + this.getComment() + " " + super.getComment();
-    if (super.getCondition() != null
-        && super.getCondition().isPresent()
-        && !super.getCondition().get().isEmpty()) {
-      result += " ==> " + super.getCondition().get();
-    }
-    return result;
+    return super.equals(obj);
   }
 }
