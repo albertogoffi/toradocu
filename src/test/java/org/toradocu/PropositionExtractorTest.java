5c162976fd6fa7814470f3bc9596f4ea2d57d688
Alberto Goffi <alberto.goffi@usi.ch>
2017-03-08 22:25:56 +0100

    Fix test cases

--- src/test/java/org/toradocu/PropositionExtractorTest.java
+++ src/test/java/org/toradocu/PropositionExtractorTest.java
@@ -3,6 +3,7 @@ package org.toradocu;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import org.jgrapht.Graph;
@@ -20,18 +21,20 @@ public class PropositionExtractorTest {
 		
 		Set<Proposition> propositions = graph.vertexSet();
 		assertEquals(2, propositions.size());
-		assertTrue(propositions.contains(new Proposition("array", "is empty")));
-		assertTrue(propositions.contains(new Proposition("size", "is zero")));
+		final Iterator<Proposition> iterator = propositions.iterator();
+		assertEquals(new Proposition("array", "is empty"), iterator.next());
+		assertEquals(new Proposition("size", "is zero"), iterator.next());
 	}
 	
 	@Test
 	public void multiplePropositions() throws Exception {
 		Graph<Proposition, ConjunctionEdge<Proposition>> graph = PropositionExtractor.getPropositionGraph("The array is empty. The size is zero");
 		assertEquals(0, graph.edgeSet().size());
-		
+
 		Set<Proposition> propositions = graph.vertexSet();
 		assertEquals(2, propositions.size());
-		assertTrue(propositions.contains(new Proposition("The array", "is empty"))); // TODO: check why here "the" is included, while is not the case in simpleProposition test
-		assertTrue(propositions.contains(new Proposition("The size", "is zero")));
+		final Iterator<Proposition> iterator = propositions.iterator();
+		assertEquals(new Proposition("The array", "is empty"), iterator.next());
+		assertEquals(new Proposition("The size", "is zero"), iterator.next());
 	}
 }
