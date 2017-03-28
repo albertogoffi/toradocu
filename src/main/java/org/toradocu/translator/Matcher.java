b08f236ed3a772b6c37bf2c3e302293e6b46ef07
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-03-28 10:10:50 +0200

    Add support for comparisons between variables (#95)
    * First attempt to translate comparision between variables. Fix some goal files.
    
    * Small refactoring in ConditionTranslator. Fix in precision/recall tests and goal file
    
    * Improve variable comparison. Update some tests in the precision/recall test suite.
    
    * Correct goal file of SimpsonIntegrator (CommonsMath3)
    
    * Improve matching of variables. Correct one goal file and tests in precision/recall test suite.
    
    * Add Javadoc in ConditionTranslator

--- src/main/java/org/toradocu/translator/Matcher.java
+++ src/main/java/org/toradocu/translator/Matcher.java
@@ -277,11 +277,14 @@ class Matcher {
     java.util.regex.Matcher isNotPattern =
         Pattern.compile(verbs + "(!=)? ?" + predicates).matcher(predicate);
 
-    java.util.regex.Matcher inequality =
+    java.util.regex.Matcher inequalityNumber =
         Pattern.compile(
                 verbs
                     + "(<=|>=|<|>|!=|==|=)? ?(-?([0-9]+|zero|one|two|three|four|five|six|seven|eight|nine))")
             .matcher(predicate);
+
+    java.util.regex.Matcher inequalityVar =
+        Pattern.compile(verbs + "(<=|>=|<|>|!=|==|=) ?(([a-zA-Z]+_?)+)").matcher(predicate);
 
     java.util.regex.Matcher instanceOf = Pattern.compile("(instanceof) (.*)").matcher(predicate);
 
@@ -346,11 +349,11 @@ class Matcher {
         default:
           predicateTranslation = null;
       }
-    } else if (inequality.find()) {
+    } else if (inequalityNumber.find()) {
       // Get the number from the last group of the regular expression.
-      String numberString = inequality.group(inequality.groupCount());
+      String numberString = inequalityNumber.group(inequalityNumber.groupCount());
       // Get the symbol from the regular expression.
-      String relation = inequality.group(2);
+      String relation = inequalityNumber.group(2);
       String numberWord = numberWordToDigit(numberString);
       int number =
           (!numberWord.equals(""))
@@ -362,6 +365,15 @@ class Matcher {
       } else {
         predicateTranslation = relation + number;
       }
+    } else if (inequalityVar.find()) {
+      // Get the variable from the last group of the regular expression.
+      String variable = inequalityVar.group(3);
+      // Get the symbol from the regular expression.
+      String relation = inequalityVar.group(2);
+      // Now we have the variable name, but who is it in the code? We'll have to find it.
+      predicateTranslation = relation + "{" + variable + "}";
+      if (predicate.contains(variable + "."))
+        predicateTranslation += predicate.substring(predicate.indexOf("."));
     } else if (predicate.equals("been set")) {
       predicateTranslation = "!=null";
     } else if (instanceOf.find()) {
