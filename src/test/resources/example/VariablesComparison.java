1ed6516bdbfd79926564c143916a89b6d1060357
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-03-31 13:49:57 +0200

    Improve inequalities handling, i.e., variables comparison (#100)
    * Small fixes for variables comparison feature
    
    * Add subject (mock) to test variables comparison feature on all tags
    
    * Fix variables comparison test

---
+++ src/test/resources/example/VariablesComparison.java
@@ -0,0 +1,56 @@
+package example;
+
+public class VariablesComparison {
+
+  public VariablesComparison() {
+    super();
+  }
+
+  /** @return true if first=second */
+  public boolean returnEqOnlyLetters(int first, int second) {
+    return first == second;
+  }
+
+  /** @return true if v1=v2 */
+  public boolean returnEqLettersNumbers(int v1, int v2) {
+    return v1 == v2;
+  }
+
+  /** @return true if v1>=v2 */
+  public boolean returnGELettersNumbers(int v1, int v2) {
+    return v1 >= v2;
+  }
+
+  /** @return true if v1 is smaller than v2 */
+  public boolean returnLTLettersNumbers(int v1, int v2) {
+    return v1 < v2;
+  }
+
+  /**
+   * @param v1 must be >= v2
+   * @param v2
+   * @return difference between v1 and v2
+   */
+  public int paramGELettersNumbers(int v1, int v2) {
+    return v1 - v2;
+  }
+
+  /**
+   * @param first
+   * @param second must be smaller than first
+   * @return difference between first and second
+   */
+  public int paramLTLetters(int first, int second) {
+    return first - second;
+  }
+
+  /** @throws IAE if v2 > v1 */
+  public int throwsGTLettersNumbers(int v1, int v2) {
+    return v1 - v2;
+  }
+
+  /** @throws IAE if first is smaller than second */
+  public int throwsLTLetters(int first, int second) {
+    return first - second;
+  }
+}
