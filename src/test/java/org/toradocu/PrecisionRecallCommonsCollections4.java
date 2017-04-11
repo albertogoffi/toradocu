c98e2b7417ab1de588a837ec32e27afad6ba05b5
Alberto Goffi <alberto.goffi@usi.ch>
2017-04-11 12:18:14 +0200

    Enable all test cases

--- src/test/java/org/toradocu/PrecisionRecallCommonsCollections4.java
+++ src/test/java/org/toradocu/PrecisionRecallCommonsCollections4.java
@@ -16,65 +16,65 @@ public class PrecisionRecallCommonsCollections4 extends AbstractPrecisionRecallT
     super(COMMONSCOLLECTIONS_4_SRC, COMMONSCOLLECTIONS_4_BIN, COMMONSCOLLECTIONS_4_GOAL_DIR);
   }
 
-  // @Test
-  // public void testArrayStack() throws Exception {
-  //   test("org.apache.commons.collections4.ArrayStack", 1, 0.75, 1, 1, 1, 1);
-  // }
+   @Test
+   public void testArrayStack() throws Exception {
+     test("org.apache.commons.collections4.ArrayStack", 1, 0.75, 1, 1, 1, 1);
+   }
 
-  // @Test
-  // public void testBagUtils() throws Exception {
-  //   test("org.apache.commons.collections4.BagUtils", 1, 1, 1, 1, 1, 1);
-  // }
+   @Test
+   public void testBagUtils() throws Exception {
+     test("org.apache.commons.collections4.BagUtils", 1, 1, 1, 1, 1, 1);
+   }
 
-  // @Test
-  // public void testClosureUtils() throws Exception {
-  //   test("org.apache.commons.collections4.ClosureUtils", 1, 0.695, 1, 0, 1, 1);
-  // }
+   @Test
+   public void testClosureUtils() throws Exception {
+     test("org.apache.commons.collections4.ClosureUtils", 1, 0.695, 1, 0, 1, 1);
+   }
 
-  // @Test
-  // public void testCollectionUtils() throws Exception {
-  //   test("org.apache.commons.collections4.CollectionUtils", 0.966, 0.935, 1, 1, 1, 0);
-  // }
+   @Test
+   public void testCollectionUtils() throws Exception {
+     test("org.apache.commons.collections4.CollectionUtils", 0.966, 0.935, 1, 1, 1, 0);
+   }
 
-  // @Test
-  // public void testPredicateUtils() throws Exception {
-  //   test("org.apache.commons.collections4.PredicateUtils", 1, 0.862, 1, 1, 1, 1);
-  // }
+   @Test
+   public void testPredicateUtils() throws Exception {
+     test("org.apache.commons.collections4.PredicateUtils", 1, 0.862, 1, 1, 1, 1);
+   }
 
-  // @Test
-  // public void testQueueUtils() throws Exception {
-  //   test("org.apache.commons.collections4.QueueUtils", 1, 1, 1, 1, 1, 1);
-  // }
+   @Test
+   public void testQueueUtils() throws Exception {
+     test("org.apache.commons.collections4.QueueUtils", 1, 1, 1, 1, 1, 1);
+   }
 
-  // @Test
-  // public void testFixedOrderComparator() throws Exception {
-  //   test("org.apache.commons.collections4.comparators.FixedOrderComparator", 1, 0.571, 1, 1, 1, 0);
-  // }
+   @Test
+   public void testFixedOrderComparator() throws Exception {
+     test("org.apache.commons.collections4.comparators.FixedOrderComparator", 1, 0.571, 1, 1, 1, 0);
+   }
 
-  // @Test
-  // public void testSynchronizedBag() throws Exception {
-  //   test("org.apache.commons.collections4.bag.SynchronizedBag", 1, 1, 1, 1, 1, 1);
-  // }
+   @Test
+   public void testSynchronizedBag() throws Exception {
+     test("org.apache.commons.collections4.bag.SynchronizedBag", 1, 1, 1, 1, 1, 1);
+   }
 
-  // @Test
-  // public void testFluentIterable() throws Exception {
-  //   test("org.apache.commons.collections4.FluentIterable", 1, 0.866, 1, 1, 1, 0);
-  // }
+   @Test
+   public void testFluentIterable() throws Exception {
+     test("org.apache.commons.collections4.FluentIterable", 1, 0.866, 1, 1, 1, 0);
+   }
 
-  // @Test
-  // public void testLRUMap() throws Exception {
-  //   test("org.apache.commons.collections4.map.LRUMap", 0.892, 0.862, 1, 0.857, 1, 0.5);
-  // }
+   @Test
+   public void testLRUMap() throws Exception {
+     test("org.apache.commons.collections4.map.LRUMap", 0.892, 0.862, 1, 0.857, 1, 0.5);
+   }
 
-  // @Test
-  // public void testAllPredicate() throws Exception {
-  //   test("org.apache.commons.collections4.functors.AllPredicate", 1, 0.75, 1, 0, 1, 1);
-  // }
+   @Test
+   public void testAllPredicate() throws Exception {
+     test("org.apache.commons.collections4.functors.AllPredicate", 1, 0.75, 1, 0, 1, 1);
+   }
 
-  // @Test
-  // public void testAndPredicate() throws Exception {
-  //   test("org.apache.commons.collections4.functors.AndPredicate", 1, 1, 1, 0, 1, 1);
-  // }
+   @Test
+   public void testAndPredicate() throws Exception {
+     test("org.apache.commons.collections4.functors.AndPredicate", 1, 1, 1, 0, 1, 1);
+   }
 
   @Test
   public void testAnyPredicate() throws Exception {
