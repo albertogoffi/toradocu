2d8bc58cc613ba8ee8e99bdbc20e590000d4b37b
Alberto Goffi <alberto.goffi@usi.ch>
2017-04-18 16:25:02 +0200

    Add routine to compute number of methods of a class

--- src/main/java/org/toradocu/util/ExecutableMembers.java
+++ src/main/java/org/toradocu/util/ExecutableMembers.java
@@ -1,8 +1,24 @@
 package org.toradocu.util;
 
 /**
- * Created by alberto on 18.04.17.
+ * Given the fully qualified name of a class, this program prints the number of public constructors
+ * and methods of the given class. Note that package-private and protected executable members are
+ * ignored.
  */
 public class ExecutableMembers {
 
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Please provide a fully qualified name of a class.");
+    }
+
+    try {
+      // We assume to have the class to load in the classpath.
+      Class<?> clazz = Class.forName(args[0]);
+      int members = clazz.getConstructors().length + clazz.getMethods().length;
+      System.out.println(members);
+    } catch (ClassNotFoundException e) {
+      throw new IllegalArgumentException("Unable to load class " + args[0], e);
+    }
+  }
 }
