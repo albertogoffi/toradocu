87744db542f9a1f43b17b1a7e02270bd250e6f49
khaeghar <sergiopnkfld@gmail.com>
2017-04-25 15:57:08 +0200

    Fixing issue #121

--- src/test/java/org/toradocu/PrecisionRecallCommonsCollections4.java
+++ src/test/java/org/toradocu/PrecisionRecallCommonsCollections4.java
@@ -33,7 +33,7 @@ public class PrecisionRecallCommonsCollections4 extends AbstractPrecisionRecallT
 
   @Test
   public void testCollectionUtils() throws Exception {
-    test("org.apache.commons.collections4.CollectionUtils", 0.966, 0.935, 1, 1, 1, 0);
+    test("org.apache.commons.collections4.CollectionUtils", 0.966, 0.935, 1, 1, 1, 0.2);
   }
 
   @Test
