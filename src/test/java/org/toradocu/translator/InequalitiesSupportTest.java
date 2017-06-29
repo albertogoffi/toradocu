84f76bd3eea61d6d11c27425f9c7bddab3c45066
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-06-29 10:52:18 +0200

    Fix comparison of JSON outputs

--- src/test/java/org/toradocu/translator/InequalitiesSupportTest.java
+++ src/test/java/org/toradocu/translator/InequalitiesSupportTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,6 +18,8 @@ import javax.tools.ToolProvider;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.toradocu.Toradocu;
+import org.toradocu.output.util.JsonOutput;
+import org.toradocu.util.GsonInstance;
 
 public class InequalitiesSupportTest {
 
@@ -53,16 +58,14 @@ public class InequalitiesSupportTest {
     Toradocu.main(argsList.toArray(new String[0]));
     assertTrue(Files.exists(actualOutput));
 
-    //    Type listType = new TypeToken<List<ExecutableMember>>() {}.getType();
-    //    try (BufferedReader actualOutputReader = Files.newBufferedReader(actualOutput);
-    //        BufferedReader expectedOutputReader = Files.newBufferedReader(actualOutput); ) {
-    //      List<ExecutableMember> actualSpecs =
-    //          GsonInstance.gson().fromJson(actualOutputReader, listType);
-    //      List<ExecutableMember> expectedSpecs =
-    //          GsonInstance.gson().fromJson(expectedOutputReader, listType);
-    //      assertThat(actualSpecs, is(expectedSpecs));
-    //    }
-    //
-    //    Files.delete(actualOutput);
+    Type listType = new TypeToken<List<JsonOutput>>() {}.getType();
+    try (BufferedReader actualOutputReader = Files.newBufferedReader(actualOutput);
+        BufferedReader expectedOutputReader = Files.newBufferedReader(actualOutput); ) {
+      List<JsonOutput> actualSpecs = GsonInstance.gson().fromJson(actualOutputReader, listType);
+      List<JsonOutput> expectedSpecs = GsonInstance.gson().fromJson(expectedOutputReader, listType);
+      assertThat(actualSpecs, is(expectedSpecs));
+    }
+
+    Files.delete(actualOutput);
   }
 }
