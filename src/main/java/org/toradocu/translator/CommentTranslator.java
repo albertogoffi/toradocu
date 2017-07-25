600ef36089e50a1de599d6afb23a38d036eda5e2
Alberto Goffi <alberto.goffi@usi.ch>
2017-07-25 22:26:02 +0200

    Add back preprocessing

--- src/main/java/org/toradocu/translator/CommentTranslator.java
+++ src/main/java/org/toradocu/translator/CommentTranslator.java
@@ -5,6 +5,7 @@ import org.toradocu.extractor.DocumentedExecutable;
 import org.toradocu.extractor.ParamTag;
 import org.toradocu.extractor.ReturnTag;
 import org.toradocu.extractor.ThrowsTag;
+import org.toradocu.translator.preprocess.PreprocessorFactory;
 import randoop.condition.specification.PostSpecification;
 import randoop.condition.specification.PreSpecification;
 import randoop.condition.specification.ThrowsSpecification;
@@ -12,14 +13,17 @@ import randoop.condition.specification.ThrowsSpecification;
 public class CommentTranslator {
 
   public static PreSpecification translate(ParamTag tag, DocumentedExecutable excMember) {
+    PreprocessorFactory.create(tag.getKind()).preprocess(tag, excMember);
     return new ParamTranslator().translate(tag, excMember);
   }
 
   public static List<PostSpecification> translate(ReturnTag tag, DocumentedExecutable excMember) {
+    PreprocessorFactory.create(tag.getKind()).preprocess(tag, excMember);
     return new ReturnTranslator().translate(tag, excMember);
   }
 
   public static ThrowsSpecification translate(ThrowsTag tag, DocumentedExecutable excMember) {
+    PreprocessorFactory.create(tag.getKind()).preprocess(tag, excMember);
     return new ThrowsTranslator().translate(tag, excMember);
   }
 }
