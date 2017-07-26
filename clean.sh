1e648f075a076762f8ad2accde608c7e1fc39faf
Alberto Goffi <alberto.goffi@usi.ch>
2017-07-26 11:58:10 +0200

    Make source code cleaner script compatible with MacOS

--- clean.sh
+++ clean.sh
@@ -1,10 +1,10 @@
 #These commands eliminate comments between Javadoc and methods in some of
 #our test sources, otherwise Javaparser won't recognize the Javadoc comment properly.
-sed -i '/@Pure/d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/*.java
-sed -i '/@SideEffectFree/d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/*.java
-sed -i '/@EnsuresQualifierIf/d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/*.java
-sed -i '258d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/RegexUtil.java
+sed -i.bak '/@Pure/d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/*.java
+sed -i.bak '/@SideEffectFree/d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/*.java
+sed -i.bak '/@EnsuresQualifierIf/d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/*.java
+sed -i.bak '258d' ./src/test/resources/src/plume-lib-1.1.0/java/src/plume/RegexUtil.java
 
-sed -i '274,282d' ./src/test/resources/src/guava-19.0-sources/com/google/common/collect/ConcurrentHashMultiset.java
+sed -i.bak '274,282d' ./src/test/resources/src/guava-19.0-sources/com/google/common/collect/ConcurrentHashMultiset.java
 
-echo "Source cleaned!"
+echo "Source cleaned! Unmodified files saved with .bak extension."
