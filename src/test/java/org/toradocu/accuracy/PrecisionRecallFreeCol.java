44c9e13f2710e5e35f0327733e7ed152dbf01023
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-07-29 10:40:38 +0200

    Fix Precision/Recall values

--- src/test/java/org/toradocu/accuracy/PrecisionRecallFreeCol.java
+++ src/test/java/org/toradocu/accuracy/PrecisionRecallFreeCol.java
@@ -20,7 +20,7 @@ public class PrecisionRecallFreeCol extends AbstractPrecisionRecallTestSuite {
 
   @Test
   public void testPlayerActivePredicate() throws Exception {
-    test("net.sf.freecol.common.model.Player$ActivePredicate", 1, 1, 1, 1, 0, 0);
+    test("net.sf.freecol.common.model.Player$ActivePredicate", 1, 1, 1, 1, 1, 1);
   }
 
   @Test
