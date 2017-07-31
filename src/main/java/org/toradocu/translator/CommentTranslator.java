7bdd5778107dd07da332c1d436fa75c7f8481973
Alberto Goffi <alberto.goffi@usi.ch>
2017-07-31 16:10:31 +0200

    Add Javadoc to CommentTranslator

--- src/main/java/org/toradocu/translator/CommentTranslator.java
+++ src/main/java/org/toradocu/translator/CommentTranslator.java
@@ -15,23 +15,53 @@ import randoop.condition.specification.PostSpecification;
 import randoop.condition.specification.PreSpecification;
 import randoop.condition.specification.ThrowsSpecification;
 
+/** Translates comments into procedure specifications. */
 public class CommentTranslator {
 
+  /**
+   * Translates the given @param comment into a precondition specification.
+   *
+   * @param tag the comment to be translated
+   * @param excMember the executable member commented with {@code tag}
+   * @return a precondition specification (an empty specification if the translation fails)
+   */
   public static PreSpecification translate(ParamTag tag, DocumentedExecutable excMember) {
     PreprocessorFactory.create(tag.getKind()).preprocess(tag, excMember);
     return new ParamTranslator().translate(tag, excMember);
   }
 
+  /**
+   * Translates the given @return comment into a list of precondition specifications.
+   *
+   * @param tag the comment to be translated
+   * @param excMember the executable member commented with {@code tag}
+   * @return a list of precondition specifications (each specification can be empty if the
+   *     translation failed)
+   */
   public static List<PostSpecification> translate(ReturnTag tag, DocumentedExecutable excMember) {
     PreprocessorFactory.create(tag.getKind()).preprocess(tag, excMember);
     return new ReturnTranslator().translate(tag, excMember);
   }
 
+  /**
+   * Translates the given @throws/@exception comment into a precondition specification.
+   *
+   * @param tag the comment to be translated
+   * @param excMember the executable member commented with {@code tag}
+   * @return a precondition specification (an empty specification if the translation fails)
+   */
   public static ThrowsSpecification translate(ThrowsTag tag, DocumentedExecutable excMember) {
     PreprocessorFactory.create(tag.getKind()).preprocess(tag, excMember);
     return new ThrowsTranslator().translate(tag, excMember);
   }
 
+  /**
+   * Creates the specifications from the comments of the given executable members.
+   *
+   * @param members the executable members whose comments have to be translated into specifications
+   * @return a map that associates each executable member (key) with its operation specification
+   *     that includes pre-, post-, and exceptional specifications.
+   */
   public static Map<DocumentedExecutable, OperationSpecification> createSpecifications(
       List<DocumentedExecutable> members) {
     Map<DocumentedExecutable, OperationSpecification> specs = new LinkedHashMap<>();
