9482c30279d655463b3e7a0bd58a29ceba3bc1cc
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-08-03 16:30:50 +0200

    Formatting

--- src/main/java/org/toradocu/translator/preprocess/MustWillShouldCanPatterns.java
+++ src/main/java/org/toradocu/translator/preprocess/MustWillShouldCanPatterns.java
@@ -1,20 +1,18 @@
 package org.toradocu.translator.preprocess;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
+import static java.util.stream.Collectors.toList;
 
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
 import org.toradocu.extractor.BlockTag;
 import org.toradocu.extractor.Comment;
 import org.toradocu.extractor.DocumentedExecutable;
 import org.toradocu.extractor.ParamTag;
 import org.toradocu.translator.Parser;
 import org.toradocu.translator.PropositionSeries;
-
-import static java.util.stream.Collectors.toList;
 
 public class MustWillShouldCanPatterns implements PreprocessingPhase {
 
@@ -23,29 +21,29 @@ public class MustWillShouldCanPatterns implements PreprocessingPhase {
     String comment = tag.getComment().getText();
     String parameterName = ((ParamTag) tag).getParameter().getName();
     String[] patterns = {
-            "must be",
-            "must not be",
-            "must never",
-            "will be",
-            "will not be",
-            "will never be",
-            "can't be",
-            "cannot be",
-            "should be",
-            "should not be",
-            "shouldn't be",
-            "may not be",
-            "Must be",
-            "Must not be",
-            "must'nt be",
-            "Will be",
-            "Will not be",
-            "Can't be",
-            "Cannot be",
-            "Should be",
-            "Should not be",
-            "Shouldn't be",
-            "May not be"
+      "must be",
+      "must not be",
+      "must never",
+      "will be",
+      "will not be",
+      "will never be",
+      "can't be",
+      "cannot be",
+      "should be",
+      "should not be",
+      "shouldn't be",
+      "may not be",
+      "Must be",
+      "Must not be",
+      "must'nt be",
+      "Will be",
+      "Will not be",
+      "Can't be",
+      "Cannot be",
+      "Should be",
+      "Should not be",
+      "Shouldn't be",
+      "May not be"
     };
     java.util.regex.Matcher matcher = Pattern.compile("\\(.*").matcher(comment);
     String separator = matcher.find() ? " " : ".";
@@ -58,41 +56,34 @@ public class MustWillShouldCanPatterns implements PreprocessingPhase {
       }
     }
 
-
     //TODO make this a preprocessing phase
     comment = comment.replace(";", ",");
     String[] beginnings = {"the", "a", "any", " the", " a", " any"};
     String commaPattern = ".*(, ?(?!may be)(?!can be)(?!could be)(?!possibly))(.*)";
-    java.util.regex.Matcher commaMatcher =
-            Pattern.compile(commaPattern).matcher(comment);
+    java.util.regex.Matcher commaMatcher = Pattern.compile(commaPattern).matcher(comment);
 
-    if(noReplacedYet && commaMatcher.find()) {
+    if (noReplacedYet && commaMatcher.find()) {
       //last group must not contain VERBS, and must contain an ADJ
       //e.g. "not null", "not empty"
       final List<PropositionSeries> extractedPropositions =
-              Parser.parse(new Comment(commaMatcher.group(2)), excMember);
+          Parser.parse(new Comment(commaMatcher.group(2)), excMember);
       final List<SemanticGraph> semanticGraphs =
-              extractedPropositions
-                      .stream()
-                      .map(PropositionSeries::getSemanticGraph)
-                      .collect(toList());
+          extractedPropositions.stream().map(PropositionSeries::getSemanticGraph).collect(toList());
       boolean goOn = false;
       for (SemanticGraph sg : semanticGraphs) {
-        if (sg.getAllNodesByPartOfSpeechPattern("VB(.*)").isEmpty() &&
-                !sg.getAllNodesByPartOfSpeechPattern("JJ(.*)").isEmpty())
-          goOn = true;
+        if (sg.getAllNodesByPartOfSpeechPattern("VB(.*)").isEmpty()
+            && !sg.getAllNodesByPartOfSpeechPattern("JJ(.*)").isEmpty()) goOn = true;
       }
 
       if (goOn) {
         int lastComma = comment.lastIndexOf(",");
-        String tokens[] = {comment.substring(0, lastComma),
-                comment.substring(lastComma + 1, comment.length())};
+        String tokens[] = {
+          comment.substring(0, lastComma), comment.substring(lastComma + 1, comment.length())
+        };
 
         for (String begin : beginnings) {
-          if (tokens[1].startsWith(begin))
-            tokens[1] = tokens[1].replaceFirst(begin, "");
+          if (tokens[1].startsWith(begin)) tokens[1] = tokens[1].replaceFirst(begin, "");
         }
-
 
         comment = tokens[0] + ". " + parameterName + " is " + tokens[1];
         noReplacedYet = false;
@@ -103,19 +94,17 @@ public class MustWillShouldCanPatterns implements PreprocessingPhase {
       String[] tokens = comment.split(" ");
       if (Arrays.asList(beginnings).contains(tokens[0])) {
         final List<PropositionSeries> extractedPropositions =
-                Parser.parse(new Comment(comment), excMember);
+            Parser.parse(new Comment(comment), excMember);
         final List<SemanticGraph> semanticGraphs =
-                extractedPropositions
-                        .stream()
-                        .map(PropositionSeries::getSemanticGraph)
-                        .collect(toList());
+            extractedPropositions
+                .stream()
+                .map(PropositionSeries::getSemanticGraph)
+                .collect(toList());
         boolean goOn = false;
         for (SemanticGraph sg : semanticGraphs) {
-          if (sg.getAllNodesByPartOfSpeechPattern("VB(.*)").isEmpty()){
+          if (sg.getAllNodesByPartOfSpeechPattern("VB(.*)").isEmpty()) {
             List<IndexedWord> adjs = sg.getAllNodesByPartOfSpeechPattern("JJ(.*)");
-            for(IndexedWord adj : adjs)
-              if(adj.word().equals(tokens[1]))
-                goOn = true;
+            for (IndexedWord adj : adjs) if (adj.word().equals(tokens[1])) goOn = true;
           }
         }
         // At this point, no verb was found. There remains
