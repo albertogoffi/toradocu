0fc8e6b974a1e8ef0dae26efd185a5409a1de9e8
Alberto Goffi <alberto.goffi@usi.ch>
2017-08-12 15:38:20 +0200

    Add Commons Collections expected files

--- src/test/java/org/toradocu/PrecisionRecallPaper.java
+++ src/test/java/org/toradocu/PrecisionRecallPaper.java
@@ -3,14 +3,16 @@ package org.toradocu;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.toradocu.random.AccuracyRandomCommonsCollections4;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    PrecisionRecallCommonsCollections4.class,
-    PrecisionRecallCommonsMath3.class,
-    PrecisionRecallGuava19.class,
-    PrecisionRecallJGraphT.class,
-    PrecisionRecallPlumeLib.class
+    AccuracyRandomCommonsCollections4.class
+//    PrecisionRecallCommonsMath3.class,
+//    PrecisionRecallFreeCol.class,
+//    PrecisionRecallGuava19.class,
+//    PrecisionRecallJGraphT.class,
+//    PrecisionRecallPlumeLib.class
 })
 public class PrecisionRecallPaper {
 
---
+++ src/test/java/org/toradocu/random/AccuracyRandomCommonsCollections4.java
@@ -0,0 +1,32 @@
+package org.toradocu.random;
+
+import org.junit.Test;
+import org.toradocu.testlib.AbstractPrecisionRecallTestSuite;
+import org.toradocu.testlib.PrecisionRecallTest;
+
+public class AccuracyRandomCommonsCollections4 extends AbstractPrecisionRecallTestSuite {
+
+  private static final String COMMONSCOLLECTIONS_4_SRC =
+      "src/test/resources/commons-collections4-4.1-src/src/main/java";
+  private static final String COMMONSCOLLECTIONS_4_EXPECTED_DIR =
+      "src/test/resources/CommonsCollections-4.1/";
+
+  @Test
+  public void ifClosureTest() throws Exception {
+    test("org.apache.commons.collections4.functors.IfClosure");
+  }
+
+  @Test
+  public void lazyMapTest() throws Exception {
+    test("org.apache.commons.collections4.map.LazyMap");
+  }
+
+  @Test
+  public void lazyIteratorChainTest() throws Exception {
+    test("org.apache.commons.collections4.iterators.LazyIteratorChain");
+  }
+
+  private void test(String clazz) {
+    PrecisionRecallTest.test(clazz, COMMONSCOLLECTIONS_4_SRC, COMMONSCOLLECTIONS_4_EXPECTED_DIR);
+  }
+}
