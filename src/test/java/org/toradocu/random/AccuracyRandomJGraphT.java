718ade9d09d1c51c7deab18505994d096ce49f10
Alberto Goffi <alberto.goffi@usi.ch>
2017-08-14 17:20:14 +0200

    Add JGraphT random test suite

---
+++ src/test/java/org/toradocu/random/AccuracyRandomJGraphT.java
@@ -0,0 +1,30 @@
+package org.toradocu.random;
+
+import org.junit.Test;
+import org.toradocu.testlib.AbstractPrecisionRecallTestSuite;
+import org.toradocu.testlib.PrecisionRecallTest;
+
+public class AccuracyRandomJGraphT extends AbstractPrecisionRecallTestSuite {
+
+  private static final String SRC = "src/test/resources/JGraphT";
+  private static final String EXPECTED = "src/test/resources/JGrapht-0.9.2/";
+
+  @Test
+  public void graphsTest() throws Exception {
+    test("org.jgrapht.Graphs");
+  }
+
+  @Test
+  public void kShortestPathsTest() throws Exception {
+    test("org.jgrapht.alg.KShortestPaths");
+  }
+
+  @Test
+  public void directedAcyclicGraphTest() throws Exception {
+    test("org.jgrapht.experimental.dag.DirectedAcyclicGraph");
+  }
+
+  private void test(String clazz) {
+    PrecisionRecallTest.test(clazz, SRC, EXPECTED);
+  }
+}
