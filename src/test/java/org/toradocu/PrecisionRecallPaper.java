52b228478152334faa95fefc2babf33e8f9ad4ee
Alberto Goffi <alberto.goffi@usi.ch>
2017-08-14 17:26:43 +0200

    Update paper test suite

--- src/test/java/org/toradocu/PrecisionRecallPaper.java
+++ src/test/java/org/toradocu/PrecisionRecallPaper.java
@@ -5,16 +5,22 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.toradocu.random.AccuracyRandomCommonsCollections4;
 import org.toradocu.random.AccuracyRandomCommonsMath3;
+import org.toradocu.random.AccuracyRandomFreeCol;
+import org.toradocu.random.AccuracyRandomGraphStream;
+import org.toradocu.random.AccuracyRandomGuava19;
+import org.toradocu.random.AccuracyRandomJGraphT;
+import org.toradocu.random.AccuracyRandomPlumeLib;
 
 @RunWith(Suite.class)
 @SuiteClasses({
     AccuracyRandomCommonsCollections4.class,
     AccuracyRandomCommonsMath3.class,
-//    PrecisionRecallFreeCol.class,
-//    PrecisionRecallGuava19.class,
-//    PrecisionRecallJGraphT.class,
-//    PrecisionRecallPlumeLib.class
-})
+    AccuracyRandomFreeCol.class,
+    AccuracyRandomGraphStream.class,
+    AccuracyRandomGuava19.class,
+    AccuracyRandomJGraphT.class,
+    AccuracyRandomPlumeLib.class
+g})
 public class PrecisionRecallPaper {
 
 }
