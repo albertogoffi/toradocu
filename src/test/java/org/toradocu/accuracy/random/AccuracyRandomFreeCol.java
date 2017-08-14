559bc0c0405870c73d9986065e3fc9ca4da9617d
ariannab <a.blasi3@campus.unimib.it>
2017-08-14 14:32:53 +0200

    Update random test suite

--- src/test/java/org/toradocu/accuracy/random/AccuracyRandomFreeCol.java
+++ src/test/java/org/toradocu/accuracy/random/AccuracyRandomFreeCol.java
@@ -1,5 +1,6 @@
 package org.toradocu.accuracy.random;
 
+import org.junit.Test;
 import org.toradocu.testlib.AbstractPrecisionRecallTestSuite;
 
 /** Created by arianna on 07/08/17. */
@@ -12,5 +13,20 @@ public class AccuracyRandomFreeCol extends AbstractPrecisionRecallTestSuite {
 
   public AccuracyRandomFreeCol() {
     super(FREECOL_SRC, FREECOL_BIN, FREECOL_GOAL_DIR);
+  }
+
+  @Test
+  public void testPlayer() throws Exception {
+    test("net.sf.freecol.common.model.Player", 1, 1, 0, 1, 0.214, 0.5);
+  }
+
+  @Test
+  public void testBuildingType() throws Exception {
+    test("net.sf.freecol.common.model.BuildingType", 1, 1, 0, 1, 0, 0);
+  }
+
+  @Test
+  public void testIndianNationType() throws Exception {
+    test("net.sf.freecol.common.model.IndianNationType", 1, 1, 1, 1, 1, 1);
   }
 }
