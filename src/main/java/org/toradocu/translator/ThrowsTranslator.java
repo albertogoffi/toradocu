c9ae5b9964e0bc9651537befc6c707a2b3ee340a
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-08-17 09:34:44 +0200

    Translate "always" condition in throws tags. Fix precision/recall values in random test suite

--- src/main/java/org/toradocu/translator/ThrowsTranslator.java
+++ src/main/java/org/toradocu/translator/ThrowsTranslator.java
@@ -8,9 +8,29 @@ import randoop.condition.specification.ThrowsSpecification;
 public class ThrowsTranslator {
 
   public ThrowsSpecification translate(ThrowsTag tag, DocumentedExecutable excMember) {
-    final String commentTranslation = BasicTranslator.translate(tag, excMember);
+    String commentTranslation = "";
+    commentTranslation =
+        alwaysThrowException(tag.getComment().getText())
+            ? "true"
+            : BasicTranslator.translate(tag, excMember);
+
     final Guard guard = new Guard(tag.getComment().getText(), commentTranslation);
     final String exceptionName = tag.getException().getName();
     return new ThrowsSpecification(tag.toString(), guard, exceptionName);
+  }
+
+  /**
+   * Returns true if an exception is always thrown by the {@code DocumentedExecutable}
+   *
+   * @param commentText the String comment belonging to the {@code ThrowsTag}
+   * @return true if the comment just states "always", false otherwise
+   */
+  private boolean alwaysThrowException(String commentText) {
+    commentText = commentText.replace(".", "");
+    if (commentText.equalsIgnoreCase("always")) {
+      return true;
+    }
+
+    return false;
   }
 }
