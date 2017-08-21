1db54d67b5998d87ba54aa62211f1b11884cd1bd
Arianna Blasi <a.blasi3@campus.unimib.it>
2017-08-21 17:56:20 +0200

    Minor improvement, fix precision expected value for FreeCol (accuracy random)

--- src/main/java/org/toradocu/translator/ParamTranslator.java
+++ src/main/java/org/toradocu/translator/ParamTranslator.java
@@ -8,8 +8,16 @@ import randoop.condition.specification.PreSpecification;
 public class ParamTranslator {
 
   public PreSpecification translate(ParamTag tag, DocumentedExecutable excMember) {
-    final String commentTranslation = BasicTranslator.translate(tag, excMember);
+    final String commentTranslation =
+        isDescriptiveComment(tag.getComment().getText())
+            ? ""
+            : BasicTranslator.translate(tag, excMember);
+
     final Guard guard = new Guard(tag.getComment().getText(), commentTranslation);
     return new PreSpecification(tag.toString(), guard);
+  }
+
+  private boolean isDescriptiveComment(String text) {
+    return text.matches("(.*) is (If|if) (.*)");
   }
 }
