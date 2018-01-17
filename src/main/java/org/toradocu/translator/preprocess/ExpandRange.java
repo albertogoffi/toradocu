089631e5c759ba93cfdf32a6f8d4ba6cc87629fe
Arianna Blasi <a.blasi3@campus.unimib.it>
2018-01-17 16:06:51 +0100

    Improve value ranges preprocessing (#196)
    * Improve range preprocessing
    
    * Add documentation and refactor the code
    
    * Fix Javadoc documentation

--- src/main/java/org/toradocu/translator/preprocess/ExpandRange.java
+++ src/main/java/org/toradocu/translator/preprocess/ExpandRange.java
@@ -1,13 +1,37 @@
 package org.toradocu.translator.preprocess;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.regex.Pattern;
 import org.toradocu.extractor.BlockTag;
 import org.toradocu.extractor.DocumentedExecutable;
 
+/**
+ * Preprocessing phase in which value ranges are replaced with standard inequalities using &lt; and
+ * &gt; math operators.
+ */
 public class ExpandRange implements PreprocessingPhase {
 
+  private static final List<String> inRangeWords = Arrays.asList("in", "inside");
+  private static final List<String> outRangeWords = Arrays.asList("out", "outside");
+  private static final String inOutGroup;
+
+  static {
+    StringJoiner joiner = new StringJoiner("|", "(", ")");
+    for (String word : inRangeWords) {
+      joiner.add(word);
+    }
+    for (String word : outRangeWords) {
+      joiner.add(word);
+    }
+    inOutGroup = joiner.toString();
+  }
+
   private static final String SQUARE_BRACKETS_RANGE =
-      "(not )?(lie )?(in|inside|outside) the .*(interval|range) (of )?\\[([0-9]+) ?,? ?([0-9]+)\\]";
+      "(not )?(lie )?"
+          + inOutGroup
+          + " the ([\\w\\s]+)?(interval|range) (of )?\\[([0-9]+) ?,? ?([0-9]+)\\]";
 
   @Override
   public String run(BlockTag tag, DocumentedExecutable excMember) {
@@ -16,14 +40,17 @@ public class ExpandRange implements PreprocessingPhase {
         Pattern.compile(SQUARE_BRACKETS_RANGE).matcher(comment);
 
     while (squareBracketsRange.find()) {
-      int min = Integer.valueOf(squareBracketsRange.group(6));
-      int max = Integer.valueOf(squareBracketsRange.group(7));
-      String predicateTranslation = "";
-      String negation = squareBracketsRange.group(1);
-      String insideRange = squareBracketsRange.group(2);
-      if (negation == null && insideRange != null) {
+      int min = Integer.valueOf(squareBracketsRange.group(7));
+      int max = Integer.valueOf(squareBracketsRange.group(8));
+      boolean negation = squareBracketsRange.group(1) != null;
+      final String insideOrOutside = squareBracketsRange.group(3);
+      boolean insideRange = inRangeWords.contains(insideOrOutside);
+      boolean outsideRange = outRangeWords.contains(insideOrOutside);
+      if (!negation && insideRange || negation && outsideRange) {
+        //covers "inside/in range" and "not out/outside range"
         comment = comment.replaceFirst(SQUARE_BRACKETS_RANGE, ">" + min + " and <" + max);
       } else {
+        //covers "not in/inside range" and "out/outside range"
         comment = comment.replaceFirst(SQUARE_BRACKETS_RANGE, "<" + min + " or >" + max);
       }
     }
