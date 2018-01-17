5a51de3cec315be248e10aee35fb0874999ef08d
Alberto Goffi <alberto.goffi@usi.ch>
2018-01-17 14:38:58 +0100

    Add documentation and refactor the code

--- src/main/java/org/toradocu/translator/preprocess/ExpandRange.java
+++ src/main/java/org/toradocu/translator/preprocess/ExpandRange.java
@@ -1,13 +1,37 @@
 package org.toradocu.translator.preprocess;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.regex.Pattern;
 import org.toradocu.extractor.BlockTag;
 import org.toradocu.extractor.DocumentedExecutable;
 
+/**
+ * Preprocessing phase in which value ranges are replaced with standard inequalities using "<" and
+ * ">" math operators.
+ */
 public class ExpandRange implements PreprocessingPhase {
 
+  private static final List<String> inRangeWords = Arrays.asList("in", "inside");
+  private static final List<String> outRangeWords = Arrays.asList("out", "outside");
+  private static final String inOutGroup;
+
+  static {
+    StringJoiner joiner = new StringJoiner("|", "(", ")");
+    for (String word : inRangeWords) {
+      joiner.add(word);
+    }
+    for (String word : outRangeWords) {
+      joiner.add(word);
+    }
+    inOutGroup = joiner.toString();
+  }
+
   private static final String SQUARE_BRACKETS_RANGE =
-      "(not )?(lie )?(in|inside|out|outside) the ([\\w\\s]+)?(interval|range) (of )?\\[([0-9]+) ?,? ?([0-9]+)\\]";
+      "(not )?(lie )?"
+          + inOutGroup
+          + " the ([\\w\\s]+)?(interval|range) (of )?\\[([0-9]+) ?,? ?([0-9]+)\\]";
 
   @Override
   public String run(BlockTag tag, DocumentedExecutable excMember) {
@@ -19,12 +43,9 @@ public class ExpandRange implements PreprocessingPhase {
       int min = Integer.valueOf(squareBracketsRange.group(7));
       int max = Integer.valueOf(squareBracketsRange.group(8));
       boolean negation = squareBracketsRange.group(1) != null;
-      boolean insideRange =
-          squareBracketsRange.group(3).equals("inside")
-              || squareBracketsRange.group(3).equals("in");
-      boolean outsideRange =
-          squareBracketsRange.group(3).equals("outside")
-              || squareBracketsRange.group(3).equals("out");
+      final String insideOrOutside = squareBracketsRange.group(3);
+      boolean insideRange = inRangeWords.contains(insideOrOutside);
+      boolean outsideRange = outRangeWords.contains(insideOrOutside);
       if (!negation && insideRange || negation && outsideRange) {
         //covers "inside/in range" and "not out/outside range"
         comment = comment.replaceFirst(SQUARE_BRACKETS_RANGE, ">" + min + " and <" + max);
