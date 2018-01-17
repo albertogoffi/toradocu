a6933b486327fd64cfe3e752e9f45a7eaae3f691
Arianna Blasi <a.blasi3@campus.unimib.it>
2018-01-17 12:49:44 +0100

    Improve range preprocessing

--- src/main/java/org/toradocu/translator/preprocess/ExpandRange.java
+++ src/main/java/org/toradocu/translator/preprocess/ExpandRange.java
@@ -7,7 +7,7 @@ import org.toradocu.extractor.DocumentedExecutable;
 public class ExpandRange implements PreprocessingPhase {
 
   private static final String SQUARE_BRACKETS_RANGE =
-      "(not )?(lie )?(in|inside|outside) the .*(interval|range) (of )?\\[([0-9]+) ?,? ?([0-9]+)\\]";
+      "(not )?(lie )?(in|inside|out|outside) the ([\\w\\s]+)?(interval|range) (of )?\\[([0-9]+) ?,? ?([0-9]+)\\]";
 
   @Override
   public String run(BlockTag tag, DocumentedExecutable excMember) {
@@ -16,14 +16,20 @@ public class ExpandRange implements PreprocessingPhase {
         Pattern.compile(SQUARE_BRACKETS_RANGE).matcher(comment);
 
     while (squareBracketsRange.find()) {
-      int min = Integer.valueOf(squareBracketsRange.group(6));
-      int max = Integer.valueOf(squareBracketsRange.group(7));
-      String predicateTranslation = "";
-      String negation = squareBracketsRange.group(1);
-      String insideRange = squareBracketsRange.group(2);
-      if (negation == null && insideRange != null) {
+      int min = Integer.valueOf(squareBracketsRange.group(7));
+      int max = Integer.valueOf(squareBracketsRange.group(8));
+      boolean negation = squareBracketsRange.group(1) != null;
+      boolean insideRange =
+          squareBracketsRange.group(3).equals("inside")
+              || squareBracketsRange.group(3).equals("in");
+      boolean outsideRange =
+          squareBracketsRange.group(3).equals("outside")
+              || squareBracketsRange.group(3).equals("out");
+      if (!negation && insideRange || negation && outsideRange) {
+        //covers "inside/in range" and "not out/outside range"
         comment = comment.replaceFirst(SQUARE_BRACKETS_RANGE, ">" + min + " and <" + max);
       } else {
+        //covers "not in/inside range" and "out/outside range"
         comment = comment.replaceFirst(SQUARE_BRACKETS_RANGE, "<" + min + " or >" + max);
       }
     }
