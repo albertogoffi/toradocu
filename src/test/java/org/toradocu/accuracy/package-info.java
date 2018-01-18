16b25afcd9a23e8033660cef6248c79912f63b1c
Alberto Goffi <alberto.goffi@usi.ch>
2018-01-18 15:35:11 +0100

    Add documentation to accuracy test packages

---
+++ src/test/java/org/toradocu/accuracy/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Precision/recall tests. Tests in this package can be used to measure the impact of a change on
+ * the accuracy of Toradocu.
+ */
+package org.toradocu.accuracy;
---
+++ src/test/java/org/toradocu/accuracy/paper/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A <b>subset</b> of the precision/recall tests in package {@code org.toradocu.accuracy}. Tests in
+ * this package are used to compute statistics for a specific publication.
+ */
+package org.toradocu.accuracy.paper;
