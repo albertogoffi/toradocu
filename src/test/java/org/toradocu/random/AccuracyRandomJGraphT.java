3cf9442c45243e3b13a13daa0a871748896cf592
Alberto Goffi <alberto.goffi@usi.ch>
2018-02-26 18:37:43 +0100

    Add JGraphT missing tests and goal files

--- src/test/java/org/toradocu/random/AccuracyRandomJGraphT.java
+++ src/test/java/org/toradocu/random/AccuracyRandomJGraphT.java
@@ -10,28 +10,58 @@ public class AccuracyRandomJGraphT extends AbstractPrecisionRecallTestSuite {
   private static final String EXPECTED = "src/test/resources/JGrapht-0.9.2/";
 
   @Test
-  public void graphsTest() throws Exception {
-    test("org.jgrapht.Graphs");
-  }
-
-  @Test
-  public void kShortestPathsTest() throws Exception {
-    test("org.jgrapht.alg.KShortestPaths");
-  }
-
-  @Test
-  public void linearGraphGeneratorTest() throws Exception {
-    test("org.jgrapht.generate.LinearGraphGenerator");
-  }
-
-  @Test
-  public void completeGraphGeneratorTest() throws Exception {
+  public void testCompleteGraphGenerator() {
     test("org.jgrapht.generate.CompleteGraphGenerator");
   }
 
   @Test
-  public void graphDelegatorTest() throws Exception {
+  public void testGraphDelegator() {
     test("org.jgrapht.graph.GraphDelegator");
+  }
+
+  @Test
+  public void testGraphs() {
+    test("org.jgrapht.Graphs");
+  }
+
+  @Test
+  public void testKShortestPaths() {
+    test("org.jgrapht.alg.KShortestPaths");
+  }
+
+  @Test
+  public void testLinearGraphGenerator() {
+    test("org.jgrapht.generate.LinearGraphGenerator");
+  }
+
+  @Test
+  public void testAbstractGraph() {
+    test("org.jgrapht.graph.AbstractGraph");
+  }
+
+  @Test
+  public void testGraph() {
+    test("org.jgrapht.Graph");
+  }
+
+  @Test
+  public void testEmptyGraphGenerator() {
+    test("org.jgrapht.generate.EmptyGraphGenerator");
+  }
+
+  @Test
+  public void testDirectedSimpleCycles() {
+    test("org.jgrapht.alg.cycle.DirectedSimpleCycles");
+  }
+
+  @Test
+  public void testAbstractPathElementList() {
+    test("org.jgrapht.alg.AbstractPathElementList");
+  }
+
+  @Test
+  public void testPatonCycleBase() {
+    test("org.jgrapht.alg.cycle.PatonCycleBase");
   }
 
   private void test(String clazz) {
