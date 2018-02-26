2f85ee04ae2ac18656c922cf97bc4159095ffe39
Alberto Goffi <alberto.goffi@usi.ch>
2018-02-26 14:16:50 +0100

    Add Commons Collections missing tests and goal files

--- src/test/java/org/toradocu/random/AccuracyRandomCommonsCollections4.java
+++ src/test/java/org/toradocu/random/AccuracyRandomCommonsCollections4.java
@@ -10,28 +10,103 @@ public class AccuracyRandomCommonsCollections4 extends AbstractPrecisionRecallTe
   private static final String EXPECTED = "src/test/resources/CommonsCollections-4.1/";
 
   @Test
-  public void ifClosureTest() throws Exception {
+  public void ifClosureTest() {
     test("org.apache.commons.collections4.functors.IfClosure");
   }
 
   @Test
-  public void lazyMapTest() throws Exception {
+  public void lazyMapTest() {
     test("org.apache.commons.collections4.map.LazyMap");
   }
 
   @Test
-  public void factoryUtilsTest() throws Exception {
+  public void factoryUtilsTest() {
     test("org.apache.commons.collections4.FactoryUtils");
   }
 
   @Test
-  public void defaultMapEntryTest() throws Exception {
+  public void defaultMapEntryTest() {
     test("org.apache.commons.collections4.keyvalue.DefaultMapEntry");
   }
 
   @Test
-  public void collectionBagTest() throws Exception {
+  public void collectionBagTest() {
     test("org.apache.commons.collections4.bag.CollectionBag");
+  }
+
+  @Test
+  public void testKeyAnalyzer() {
+    test("org.apache.commons.collections4.trie.KeyAnalyzer");
+  }
+
+  @Test
+  public void testHashedMap() {
+    test("org.apache.commons.collections4.map.HashedMap");
+  }
+
+  @Test
+  public void testArrayStack() {
+    test("org.apache.commons.collections4.ArrayStack");
+  }
+
+  @Test
+  public void testBagUtils() {
+    test("org.apache.commons.collections4.BagUtils");
+  }
+
+  @Test
+  public void testClosureUtils() {
+    test("org.apache.commons.collections4.ClosureUtils");
+  }
+
+  @Test
+  public void testCollectionUtils() {
+    test("org.apache.commons.collections4.CollectionUtils");
+  }
+
+  @Test
+  public void testPredicateUtils() {
+    test("org.apache.commons.collections4.PredicateUtils");
+  }
+
+  @Test
+  public void testQueueUtils() {
+    test("org.apache.commons.collections4.QueueUtils");
+  }
+
+  @Test
+  public void testFixedOrderComparator() {
+    test("org.apache.commons.collections4.comparators.FixedOrderComparator");
+  }
+
+  @Test
+  public void testSynchronizedBag() {
+    test("org.apache.commons.collections4.bag.SynchronizedBag");
+  }
+
+  @Test
+  public void testFluentIterable() {
+    test("org.apache.commons.collections4.FluentIterable");
+  }
+
+  @Test
+  public void testLRUMap() {
+    test("org.apache.commons.collections4.map.LRUMap");
+  }
+
+  @Test
+  public void testAllPredicate() {
+    test("org.apache.commons.collections4.functors.AllPredicate");
+  }
+
+  @Test
+  public void testAndPredicate() {
+    test("org.apache.commons.collections4.functors.AndPredicate");
+  }
+
+  @Test
+  public void testAnyPredicate() {
+    test("org.apache.commons.collections4.functors.AnyPredicate");
   }
 
   private void test(String clazz) {
